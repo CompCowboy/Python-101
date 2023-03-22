304d452d3f27521fd00b4f5bf738feb3364887ab
CompCowboy <128619304+CompCowboy@users.noreply.github.com>
2023-03-22 11:42:18 -0400

    Create manihategithub.py

---
+++ manihategithub.py
@@ -0,0 +1,7 @@
+set_numberOne = {'China', 'Potatos', 'Turkey', 'Iran', 'France', 'Corn'}
+set_numberTwo = {'Hungary', 'Corn', 'France', 'Italy', 'Swizterland', 'Denmark'}
+
+print(set.intersection(set_numberOne,set_numberTwo))
+print(set.union(set_numberOne, set_numberTwo))
+print(set.difference(set_numberOne, set_numberTwo))
+print(set_numberOne.symmetric_difference(set_numberTwo))
